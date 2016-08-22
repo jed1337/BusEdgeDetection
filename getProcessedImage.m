c017cb5b9ca4559da328318f80bb7c70b373c59d
jed1337 <j.caychingco@yahoo.com>
2016-08-22 14:25:18 +0800

    Less comments

--- getProcessedImage.m
+++ getProcessedImage.m
@@ -1,14 +1,5 @@
 function croppedImage = getProcessedImage(I)
-%    figure;
-%    imshowpair(RGB, I, 'montage');
-%    title('Coloured and grey-scale input');
-%    linkaxes;
-   % imshow(I);
-   % figure('units','normalized','outerposition',[0 0 1 1]);
-
    [y, x] = size(I);
-   % figure;
-%    BW1 = edge(I,'Sobel', [], 'both');
    BW2 = edge(I,'Sobel', [], 'vertical');
    BW3 = edge(I,'Sobel', [], 'horizontal');
 
@@ -20,12 +11,7 @@ function croppedImage = getProcessedImage(I)
 
    BW2 = bwareaopen(BW2, iY);
    BW3 = bwareaopen(BW3, iX);
-
-%    figure;
-%    imshowpair(BW2, BW3, 'montage');
-%    title(sprintf('Vertical & Horizontal. iX: %d iY: %d',iX, iY));
-%    linkaxes;
-% 
+   
 %    figure;
 %    title('Vertical & Horizontal convex hull');
 %    imshowpair(bwconvhull(BW2), bwconvhull(BW3), 'montage');
@@ -36,7 +22,4 @@ function croppedImage = getProcessedImage(I)
    croppedImage = imcrop(I, boundingBox);
    [cy, cx] =size(croppedImage);
    croppedImage = croppedImage(floor(cy/2):cy, :, 1);
-%    bottomHalf = croppedImage(floor(cy/2):cy, :, 1);
-%    figure;
-%    imshowpair(croppedImage, bottomHalf, 'montage');
 end
